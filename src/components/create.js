b3f62f5e756c1a7ffc5af417e30ac9cbb7bf5fb5
Ryan Hogan <ryanoman2011@hotmail.com>
2023-10-13 13:08:11 +0100

    Added create and read components.

---
+++ src/components/create.js
@@ -0,0 +1,9 @@
+function Create(){
+    return(
+        <div>
+            <h3>Hello from create.js component.</h3>
+        </div>
+    );
+}
+
+export default Create;
---
+++ src/components/read.js
@@ -0,0 +1,9 @@
+function Read(){
+    return(
+        <div>
+            <h3>Hello from read.js component.</h3>
+        </div>
+    );
+}
+
+export default Read;
