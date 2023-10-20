4c7f7a8b8da1a2c17c3778a5cec998b75219563e
Ryan Hogan <ryanoman2011@hotmail.com>
2023-10-20 15:12:25 +0100

    Finished create component.

--- src/components/create.js
+++ src/components/create.js
@@ -1,7 +1,62 @@
+import { useState } from "react";
+
 function Create(){
+
+    const [title, setTitle] = useState('');
+    const [cover, setCover] = useState('');
+    const [author, setAuthor] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Title: " + title +
+                    " Cover: " + cover +
+                    " Author: " + author);
+    }
+
     return(
         <div>
             <h3>Hello from create.js component.</h3>
+            <form onSubmit = {handleSubmit}>
+                <div className = "form-group">
+                    <label>Add Book Title: </label>
+                    <input type = "text"
+                        className = "form-control"
+                        value = {title}
+                        onChange = {
+                            (e) => { 
+                                setTitle(e.target.value) 
+                            }
+                        }
+                    />
+                </div>
+                <div className = "form-group">
+                    <label>Add Book Author: </label>
+                    <input type = "text"
+                        className = "form-control"
+                        value = {author}
+                        onChange = {
+                            (e) => { 
+                                setAuthor(e.target.value) 
+                            }
+                        }
+                    />
+                </div>
+                <div className = "form-group">
+                    <label>Add Book Cover: </label>
+                    <input type = "text"
+                        className = "form-control"
+                        value = {cover}
+                        onChange = {
+                            (e) => { 
+                                setCover(e.target.value) 
+                            }
+                        }
+                    />
+                </div>
+                <div>
+                    <input type = "submit" value = "Add Book"></input>
+                </div>
+            </form>
         </div>
     );
 }
